184e4c5ad071d9cc61e5a808f8ffcde180dd8b78
philstopford <phil.stopford@gmail.com>
2021-10-14 17:39:31 -0500

    Build fixes and package updates

--- Quilt/quilt/QuiltContext.cs
+++ Quilt/quilt/QuiltContext.cs
@@ -44,7 +44,7 @@ namespace Quilt
             angularTolerance = 0.2;
             colors = new Colors();
             backend = _backend;
-            licenceName = "internal";
+            licenceName = "GPLv3";
         }
     }
 }
